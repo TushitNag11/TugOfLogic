27aa5d06c6c1b00b24a34edb372c7ac72a7e0358
Tushit Nag <tushitnag@gmail.com>
2020-10-02 13:51:39 -0700

    started ui

--- app/src/main/java/com/example/tugoflogic/MainActivity.kt
+++ app/src/main/java/com/example/tugoflogic/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.tugoflogic
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
+        } else if (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
+        }
     }
 }
