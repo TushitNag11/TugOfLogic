b69a68f52773692279e0864cefbc546e38e17a2a
Tushit Nag <tushitnag@gmail.com>
2020-10-04 04:23:30 -0700

    signup and login linked

--- app/src/main/java/com/example/tugoflogic/MainActivity.kt
+++ app/src/main/java/com/example/tugoflogic/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.tugoflogic
 
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.signupBtn)
+        button.setOnClickListener{
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+        }
 
 
     }
