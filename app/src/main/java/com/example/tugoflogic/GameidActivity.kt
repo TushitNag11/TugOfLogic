706999387d795511c4bd3a37952677cc94505b33
Tushit Nag <tushitnag@gmail.com>
2020-10-04 11:54:05 -0700

    ui changes

---
+++ app/src/main/java/com/example/tugoflogic/GameidActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugoflogic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gameidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameid)
+    }
+}
