9446ad43d0e6aeb3afb92ce3a7fb339eec3f2002
nam <mostman47@yahoo.com>
2020-10-06 00:31:55 -0700

    add author

--- app/src/main/java/com/example/tugoflogic/controller/GameController.java
+++ app/src/main/java/com/example/tugoflogic/controller/GameController.java
@@ -3,7 +3,7 @@ package com.example.tugoflogic.controller;
 /**
  * Main game controller controls all actions & logic of the game
  *
- * @author
+ * @author nam
  * @version 1.0
  * @since   2020-10-06
  */
