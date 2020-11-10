b4591435e736e4f797e7088becd32010a7ed3c1b
Tushit Nag <tushitnag@gmail.com>
2020-11-09 20:35:24 -0800

    Made some New UI

--- app/src/main/java/com/example/tugoflogic/MainClaimActivity.kt
+++ app/src/main/java/com/example/tugoflogic/MainClaimActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tugoflogic
 
+import android.content.Intent
 import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main_claim.*
 
 /**
@@ -16,6 +18,13 @@ class MainClaimActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_claim)
+
+        val submitBtn = findViewById<Button>(R.id.submitBtn)
+        submitBtn.setOnClickListener {
+
+            val intent = Intent(this, ReasonInPlay_User::class.java)
+            startActivity(intent)
+        }
 
            }
 }
---
+++ app/src/main/java/com/example/tugoflogic/ReasonInPlay_User.kt
@@ -0,0 +1,11 @@
+package com.example.tugoflogic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReasonInPlay_User : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reason_in_play__user)
+    }
+}
