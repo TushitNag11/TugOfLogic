1aa69bd68cc6aa6374adfb4d3b44f0fe6e230a07
Tushit Nag <tushitnag@gmail.com>
2020-11-23 20:02:47 -0800

    updated Admin UI

---
+++ app/src/main/java/com/example/tugoflogic/Admin/ripVotingResultsAdmin.kt
@@ -0,0 +1,12 @@
+package com.example.tugoflogic.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugoflogic.R
+
+class ripVotingResultsAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rip_voting_results_admin)
+    }
+}
