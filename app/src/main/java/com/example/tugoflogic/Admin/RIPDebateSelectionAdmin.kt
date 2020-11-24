48dc0f04507a485d981a86e5f9d21825cc10f625
Tushit Nag <tushitnag@gmail.com>
2020-11-23 16:02:51 -0800

    added admin UI

---
+++ app/src/main/java/com/example/tugoflogic/Admin/RIPDebateSelectionAdmin.kt
@@ -0,0 +1,12 @@
+package com.example.tugoflogic.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugoflogic.R
+
+class RIPDebateSelectionAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_r_i_p_debate_selection_admin)
+    }
+}
