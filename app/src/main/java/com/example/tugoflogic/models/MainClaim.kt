42374d46200145cb19dea0575c0b4d7fe5629007
Karan-95 <karan.singh.990@live.com>
2020-11-23 22:07:32 -0800

    ViewModel #2

--- app/src/main/java/com/example/tugoflogic/models/MainClaim.kt
+++ app/src/main/java/com/example/tugoflogic/models/MainClaim.kt
@@ -1,21 +1,12 @@
 package com.example.tugoflogic.models
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 
-
-public class MainClaim(application: Application) : AndroidViewModel(application)  {
-
-    var _id :  Int
-    var game_id : Int
-    var user_id : Int
+public class MainClaim(
+    var _id: Int,
+    var game_id: Int,
+    var user_id: Int,
     var statement: String
+) {
 
-    init {
-        _id = 0
-        game_id = 0
-        user_id = 0
-        statement = ""
-    }
 
 }
